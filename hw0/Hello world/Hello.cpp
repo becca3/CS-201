7a7701824e838872aad7fb91ac86d66a9479a9d3
becca3 <32517979+becca3@users.noreply.github.com>
2019-08-30 11:27:06 -0800

    Hello world program for hw0

---
+++ hw0/Hello world/Hello.cpp
@@ -0,0 +1,13 @@
+/**
+Program: Hello world
+Author: Rebecca Morgan
+Date: 30 August 2019
+Description: Hello world program for CS-201
+*/
+
+#include <iostream>
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World!\n" << std::endl;
+	return 0;
+}
