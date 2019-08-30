7a8306a9cf89807bc9bd077f32c4934f6b2a1639
becca3 <32517979+becca3@users.noreply.github.com>
2019-08-30 12:28:56 -0800

    Added comments to the code for future reference

--- hw0/Hello world/Hello.cpp
+++ hw0/Hello world/Hello.cpp
@@ -10,10 +10,10 @@ Description: Hello world program for CS-201
 int main(int argc, char** argv)
 {
 	int num = 0;
-		while (num < 5) 
+		while (num < 5) //This while loop prints "Hello world" while the num is less than 5 - once num reaches 5 the loop ends.
 		{
 			std::cout << "Hello world\n";
-			num++;
+			num++; //This will increment each time the loop runs till the num variable is no longer less than 5 
 	    }
-	return 0;
+	return 0; 
 }
