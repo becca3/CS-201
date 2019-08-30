11357e9b5bb123349a7fd09f9c7f802e1d4c18f1
becca3 <32517979+becca3@users.noreply.github.com>
2019-08-30 11:24:18 -0800

    Added poem project for hw0

---
+++ hw0/Poem/Poem.cpp
@@ -0,0 +1,29 @@
+/**
+Program: Poem
+Author: Rebecca Morgan
+Date: 30 August 2019
+Description: Poem printing project for CS-201 hw0
+*/
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+	std::string poem("The Polar Bear never makes his bed;\n"
+		"He sleeps on a cake of ice instead.\n"
+		"He has no blanket, no quilt, no sheet\n"
+		"Except the rainand snowand sleet.\n"
+		"He drifts about on a white ice floe\n"
+		"While cold winds howland blizzards blow\n"
+		"And the temperature drops to forty below.\n"
+		"The Polar Bear never makes his bed;\n"
+		"The blanket he pulls up over his head\n"
+		"Is lined with softand feathery snow.\n"
+		"If ever he roseand turned on the light,\n"
+		"He would find a world of bathtub white,\n"
+		"And icebergs floating through the night.\n"
+		"\n"
+		"Author: William Jay Smith.\n");
+	std::cout << poem << std::endl;
+}
