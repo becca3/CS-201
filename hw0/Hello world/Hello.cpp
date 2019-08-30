b33537aa9d8f2cfe9608e474494e43316450da80
becca3 <32517979+becca3@users.noreply.github.com>
2019-08-30 11:46:59 -0800

    Hello world program for hw0

--- hw0/Hello world/Hello.cpp
+++ hw0/Hello world/Hello.cpp
@@ -6,8 +6,14 @@ Description: Hello world program for CS-201
 */
 
 #include <iostream>
+
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World!\n" << std::endl;
+	int num = 0;
+		while (num < 5) 
+		{
+			std::cout << "Hello world\n";
+			num++;
+	    }
 	return 0;
 }
