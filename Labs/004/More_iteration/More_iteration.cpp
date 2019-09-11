331b3996dc4665889ed54a8fbb58b1b739225715
becca3 <32517979+becca3@users.noreply.github.com>
2019-09-11 10:14:46 -0800

    Lab 004

--- Labs/004/More_iteration/More_iteration.cpp
+++ Labs/004/More_iteration/More_iteration.cpp
@@ -1,1 +1,55 @@
 #include <iostream>
+
+void part1()
+{
+	for (int num = 1; num <= 100; num++)
+		if (num % 3 == 0)
+		{
+			std::cout << "Fizz" << "\n";
+		}
+		else
+		{
+			std::cout << num << "\n";
+		}
+	std::cout << "\n";
+}
+
+void part2()
+{
+	for (int num = 1; num <= 100; num++)
+		if (num % 3 == 0)
+		{
+			std::cout << "Fizz" << "\n";
+		}
+		else if (num % 5 == 0)
+		{
+			std::cout << "Buzz" << "\n";
+		}
+		else
+		{
+			std::cout << num << "\n";
+		}
+	std::cout << "\n";
+}
+
+void part3()
+{
+	for (int num = 1; num <= 100; num++)
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			std::cout << "Fizz Buzz" << "\n";
+		}
+		else
+		{
+			std::cout << num << "\n";
+		}
+	std::cout << "\n";
+}
+
+int main(int argc, char** argv)
+{
+	part1();
+	part2();
+	part3();
+	return 0;
+}
