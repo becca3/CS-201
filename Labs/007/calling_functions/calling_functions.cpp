3d1c02d9cdbb20c3a5fed2b181ff14cf2167ff8b
becca3 <32517979+becca3@users.noreply.github.com>
2019-09-18 10:08:59 -0800

    Completed lab 007 1st part

---
+++ Labs/007/calling_functions/calling_functions.cpp
@@ -0,0 +1,21 @@
+/**
+Program: calling_funtions.cpp
+Author: Rebecca Morgan
+Date: 18 September 2019
+Description: Calling functions in a separate file program for CS-201
+*/
+
+#include "intio.cpp"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int x = getInt();
+	int y = getInt();
+	int z = getInt();
+
+	putInt(x, 10);
+	putInt(y, 10);
+	putInt(z, 10);
+}
+
