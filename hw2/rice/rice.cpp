5679e3a04405e7d03f57c5c515b3af0141399a05
becca3 <32517979+becca3@users.noreply.github.com>
2019-09-18 09:35:40 -0800

    Completed first part of program

--- hw2/rice/rice.cpp
+++ hw2/rice/rice.cpp
@@ -10,16 +10,27 @@ Description: program for CS-201
 int main(int argc, char** argv)
 {
 	int total_grains = 0;
+	int grains = 1;
+	int squares = 2;
 
-	for (int squares = 1; squares <= 64; squares++)
+	std::cout << "Square = " << "1" << "\n";
+	std::cout << "Grains = " << "1" << "\n";
+	std::cout << "\n";
+
+	for (squares; squares <= 64; squares++)
 	{
-		int grains = 1;
+		//int grains = 1;
 		total_grains = total_grains + grains;
+		grains = (grains * 2);
+		//std::cout << "---" << grains << "\n";
 		std::cout << "Square = " << squares << "\n";
-		std::cout << "Grains = " << total_grains << "\n";
-		grains = grains * 2;
+		std::cout << "Grains = " << grains << "\n";
+		std::cout << "\n";
+
+		//grains = grains * 2;
 	}
-	std::cout << total_grains;
+	std::cout << "\n";
+	std::cout << grains;
 	return 0;
 }
 
