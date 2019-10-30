021bc2d580f4314479ec3e3a31f8ba8f41a659fe
becca3 <32517979+becca3@users.noreply.github.com>
2019-10-30 09:31:40 -0800

    Created hw5 files

--- Library test/Test_lib/fltk-trunc.cpp
+++ Library test/Test_lib/fltk-trunc.cpp
@@ -1,11 +1,15 @@
 #include<FL/Fl.h>
 #include<FL/Fl_Box.h>
 #include<FL/Fl_Window.h>
+#include "truncstruct.hpp"
 
 int main()
 {
 	Fl_Window window(200, 200, "Window title");
-	Fl_Box box(0, 0, 200, 200, "Hey, I mean, Hello, World!");
+	Fl_Box box(0, 0, 200, 200, "Hey, Hello, World!");
+	Fl_Box box2(2, 20, 100, 100, "New button");
 	window.show();
 	return Fl::run();
 }
+
+//include and library directories - add lib and include folders  
