e01fd5fd74ee9a1fb97bc3c0c2e7fc7d67b51cda
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-17 14:38:28 -0900

    Changes to lots of code

--- hw6/random-map/random-map.cpp
+++ hw6/random-map/random-map.cpp
@@ -4,84 +4,93 @@
 #include <map>
 #include <random>
 #include <cmath>
-#include<ctime>
-#include<stdlib.h>
-#include<utility>
+#include <stdlib.h>
 
-int RandomBetweenU(int first, int last);
-int RandomBetweenN(int first, int last);
+int RandomBetweenU(int first, int last, std::mt19937& r1);
+int RandomBetweenN(int first, int last, std::mt19937& r1);
 int RandomBetween(int first, int last);
 void PrintDistribution(const std::map<int, int>& numbers);
 
-std::map<int, int> numbers;
-
-int RandomBetweenU(int first, int last)
-{
-	std::random_device r2;
-	std::mt19937 gen(r2());
-	std::uniform_int_distribution<int> dist(first, last);
-
-	/*for (int i = first; i <= last; i++)
-	{ 
-		std::cout << "Numbers are: :" << i << "\n";
-	}*/
-
-	std::cout << "Mean is: " << dist(gen) << "\n";
-	std::cout << "\n";
-	return dist(gen); 
-}
-
-
-int RandomBetweenN(int first, int last)
-{
-	std::random_device r2;
-	std::mt19937 gen (r2());
-	std::normal_distribution<double> dist (first, last);
-	
-	/*for (int i = first; i <= last; i++)
-	{
-		std::cout << "Normal dist: " << dist(gen) << "\n";
-	}*/
-
-	return dist(gen);
-	std::cout << "\n";
-}
-
-int RandomBetween(int first, int last)
-{
-	srand(time(NULL));
-	int random = (std::rand() % last) + 1;
-	//std::cout << "Number is: " << random << "\n";
-	return random;
-	std::cout << "\n";
-}
-
-void PrintDistribution(const std::map<int, int>& numbers)
-{
-	/*for (int n = 0; n < 10000; ++n) 
-	{
-		++numbers[std::round(dist(gen))];
-	}
-	std::cout << "Normal distribution around " << dist(gen) << ":\n";
-	for (auto p : numbers)
-	{
-		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
-	}*/
-}
-
 int main()
 {
-	int first;
-	int last; 
+	// Declare endpoints of the range for the distributions
 
-	std::cout << "Enter a lower bound number: " << "\n";
-	std::cin >> first;
-	std::cout << "Enter an upper bound number: " << "\n";
-	std::cin >> last;
+	int first = 1;
+	int last = 6;
 
-	RandomBetweenU(first, last);
-	RandomBetweenN(first, last);
-	RandomBetween(first, last);
-	PrintDistribution(numbers);
+	// Declare pseudo-random device for creating seeds
+	std::random_device r;
+
+	// Create a seed sequence to feed to the generator
+	std::seed_seq seedObj{ r(), r(), r(), r(), r(), r(), r(), r() };
+
+	// Declare random-number generator and provide with seedObj sequence
+	std::mt19937 r1(seedObj);
+
+	// Create uniform historgram
+	std::map<int, int> uniformHistogram;
+	for (int i = 0; i < 10000; i++) 
+	{
+		++uniformHistogram[std::round(RandomBetweenU(first, last, r1))];
+	}
+
+	// Print the uniform histogram
+	PrintDistribution(uniformHistogram);
+
+	// Create normal histogram
+	std::map<int, int> normalHistogram;
+	for (int i = 0; i < 10000; i++) 
+	{
+		++normalHistogram[std::round(RandomBetweenN(first, last, r1))];
+	}
+
+	// Print the normal histogram
+	PrintDistribution(normalHistogram);
+
+	// Create standard rand() histogram
+	std::map<int, int> standardHistogram;
+	for (int i = 0; i < 10000; i++) 
+	{
+		++standardHistogram[std::round(RandomBetween(first, last))];
+	}
+
+	// Print the normal histogram
+	PrintDistribution(standardHistogram);
+
+	return 0;
+}
+
+int RandomBetweenU(int first, int last, std::mt19937& r1) 
+{
+
+	std::uniform_int_distribution<int> uniform_dist(first, last);
+	int ran = uniform_dist(r1);
+
+	return ran;
+}
+
+int RandomBetweenN(int first, int last, std::mt19937& r1) 
+{
+	std::normal_distribution<> normal_dist(2.5, 2);
+	int ran = normal_dist(r1);
+
+	return ran;
+}
+
+int RandomBetween(int first, int last) 
+{
+	int ran = (std::round(std::rand()));
+	ran = ran % 6;
+	return ran;
+}
+
+void PrintDistribution(const std::map<int, int>& numbers) 
+{
+	for (auto p : numbers)
+	{
+		std::cout << std::fixed << std::setprecision(1) 
+			<< std::setw(2) << p.first << ' ' << std::string(p.second / 20, '*') 
+			<< std::endl;
+	}
+	std::cout << std::endl;
 }
