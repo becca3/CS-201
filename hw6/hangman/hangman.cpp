3d85ac4588bec02549adfe3a91563d562fea26a3
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-17 13:58:36 -0900

    Changed some functions

--- hw6/hangman/hangman.cpp
+++ hw6/hangman/hangman.cpp
@@ -1,33 +1,47 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include<map>
+#include <map>
 
-
-int count = 0;
 std::string guess;
 std::map<std::string, int> guesses;
 std::string word = "dinosaur";
 
+//std::transform(word.begin(), word.end(), sln.begin(), [&](auto i) 
+//	{
+//		return word[i] == guess;
+//	});
+
 
 int main(int argc, char** argv)
 {
-	std::cout << "Guess a letter: " << "\n";
-	std::cin >> guess;
-
-	for (int i = 0; i <= 10; i++)
+	while (true)
 	{
-		guesses.insert(std::pair<std::string, int>(guess, i));
-
-		for (int c = 0; c <= word.length(); c++)
+		int life = 0;
+		if (life <= 10)
 		{
-			if (word[c] == guess)
-			{
+			std::cout << "Guess a letter: " << "\n";
+			std::getline(std::cin, guess);
+		}
+		else
+		{
+			std::cout << "You died!" << std::endl;
+			break;
+		}
 
+		for (int i = 0; i <= 10; i++)
+		{
+			guesses.insert(std::pair<std::string, int>(guess, i));
+
+			for (int c = 0; c <= word.length(); c++)
+			{
+				/*if (word[c] == guess)
+				{
+
+				}*/
 			}
 		}
+		//	guesses.push_back(guess);
 	}
-	//	guesses.push_back(guess);
- 
 	return 0;
 }
--- hw6/random-map/random-map.cpp
+++ hw6/random-map/random-map.cpp
@@ -17,27 +17,32 @@ std::map<int, int> numbers;
 
 int RandomBetweenU(int first, int last)
 {
-	//std::map<int, int> numbers;
-
 	std::random_device r2;
 	std::mt19937 gen(r2());
 	std::uniform_int_distribution<int> dist(first, last);
-	//std::cout << "Mean is: " << dist(gen) << "\n";
 
-	for (int i = first; i <= last; i++)
-	{
+	/*for (int i = first; i <= last; i++)
+	{ 
+		std::cout << "Numbers are: :" << i << "\n";
+	}*/
 
-	}
-
+	std::cout << "Mean is: " << dist(gen) << "\n";
 	std::cout << "\n";
 	return dist(gen); 
 }
+
 
 int RandomBetweenN(int first, int last)
 {
 	std::random_device r2;
 	std::mt19937 gen (r2());
 	std::normal_distribution<double> dist (first, last);
+	
+	/*for (int i = first; i <= last; i++)
+	{
+		std::cout << "Normal dist: " << dist(gen) << "\n";
+	}*/
+
 	return dist(gen);
 	std::cout << "\n";
 }
@@ -78,29 +83,5 @@ int main()
 	RandomBetweenU(first, last);
 	RandomBetweenN(first, last);
 	RandomBetween(first, last);
-
-	////Seed with a real random value, if available
-	//std::random_device r;
-
-	//// Choose a random mean between 1 and 6
-	//std::default_random_engine e1(r());
-	//std::uniform_int_distribution<int> uniform_dist(1, 6);
-	//int mean = uniform_dist(e1);
-	//std::cout << "Randomly-chosen mean: " << mean << '\n';
-
-	//// Generate a normal distribution around that mean
-	//std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
-	//std::mt19937 e2(seed2);
-	//std::normal_distribution<> normal_dist(mean, 2);
-
-	//std::map<int, int> hist;
-	//for (int n = 0; n < 10000; ++n) 
-	//{
-	//	++hist[std::round(normal_dist(e2))];
-	//}
-	//std::cout << "Normal distribution around " << mean << ":\n";
-	//for (auto p : hist) {
-	//	std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-	//		<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
-	//}
+	PrintDistribution(numbers);
 }
