88a2ebe78b385d20b901c304d65768ee67c589c4
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-17 16:01:02 -0900

    Made function to print guesses and to display full word

--- hw6/hangman/hangman.cpp
+++ hw6/hangman/hangman.cpp
@@ -2,46 +2,57 @@
 #include <string>
 #include <algorithm>
 #include <map>
+#include <vector>
 
 std::string guess;
-std::map<std::string, int> guesses;
-std::string word = "dinosaur";
+std::map<int, std::string> guesses;
+std::vector<std::string> words{ "d", "i", "n", "o", "s", "a", "u", "r" };
+//std::string word = "dinosaur";
+int life = 0;
 
-//std::transform(word.begin(), word.end(), sln.begin(), [&](auto i) 
-//	{
-//		return word[i] == guess;
-//	});
-
+void printGuess(std::vector<std::string> words);
 
 int main(int argc, char** argv)
 {
-	while (true)
+	while (life <= 10)
 	{
-		int life = 0;
-		if (life <= 10)
+		std::cout << "Guess a letter: " << "\n";
+		std::getline(std::cin, guess);
+		life++;
+		guesses.insert(std::pair<int, std::string>(life, guess));
+
+		std::cout << "Guesses so far: \n";
+		std::map<int, std::string>::iterator it = guesses.begin();
+		for (it = guesses.begin(); it != guesses.end(); ++it)
+			std::cout << it->first << " - " << it->second << '\n';
+
+		if (life == 10)
 		{
-			std::cout << "Guess a letter: " << "\n";
-			std::getline(std::cin, guess);
-		}
-		else
-		{
+			std::cout << "\n";
 			std::cout << "You died!" << std::endl;
 			break;
 		}
-
-		for (int i = 0; i <= 10; i++)
+		else
 		{
-			guesses.insert(std::pair<std::string, int>(guess, i));
-
-			for (int c = 0; c <= word.length(); c++)
-			{
-				/*if (word[c] == guess)
-				{
-
-				}*/
-			}
+			printGuess(words);
 		}
-		//	guesses.push_back(guess);
 	}
 	return 0;
+}
+
+
+void printGuess(std::vector<std::string> words)
+{
+	// Lambda expression to print vector containing the word.
+	for_each (words.begin(), words.end(), [](std::string i)
+		{
+			std::cout << i << " ";
+		});
+	std::cout << std::endl;
+}
+
+void findLetter()
+{
+
+	std::vector<int>::iterator it;
 }
--- hw6/random-map/random-map.cpp
+++ hw6/random-map/random-map.cpp
@@ -6,8 +6,12 @@
 #include <cmath>
 #include <stdlib.h>
 
-int RandomBetweenU(int first, int last, std::mt19937& r1);
-int RandomBetweenN(int first, int last, std::mt19937& r1);
+using std::cout;
+using std::cin;
+using std::endl;
+
+int RandomBetweenU(int first, int last, std::mt19937& e1);
+int RandomBetweenN(int first, int last, std::mt19937& e1);
 int RandomBetween(int first, int last);
 void PrintDistribution(const std::map<int, int>& numbers);
 
@@ -25,13 +29,12 @@ int main()
 	std::seed_seq seedObj{ r(), r(), r(), r(), r(), r(), r(), r() };
 
 	// Declare random-number generator and provide with seedObj sequence
-	std::mt19937 r1(seedObj);
+	std::mt19937 e1(seedObj);
 
 	// Create uniform historgram
 	std::map<int, int> uniformHistogram;
-	for (int i = 0; i < 10000; i++) 
-	{
-		++uniformHistogram[std::round(RandomBetweenU(first, last, r1))];
+	for (int i = 0; i < 10000; i++) {
+		++uniformHistogram[std::round(RandomBetweenU(first, last, e1))];
 	}
 
 	// Print the uniform histogram
@@ -39,9 +42,14 @@ int main()
 
 	// Create normal histogram
 	std::map<int, int> normalHistogram;
-	for (int i = 0; i < 10000; i++) 
-	{
-		++normalHistogram[std::round(RandomBetweenN(first, last, r1))];
+	for (int i = 0; i < 10000; i++) {
+		int val;
+		while (true) {
+			val = std::round(RandomBetweenN(first, last, e1));
+			if (val <= last && val >= first)
+				break;
+		}
+		++normalHistogram[val];
 	}
 
 	// Print the normal histogram
@@ -49,8 +57,7 @@ int main()
 
 	// Create standard rand() histogram
 	std::map<int, int> standardHistogram;
-	for (int i = 0; i < 10000; i++) 
-	{
+	for (int i = 0; i < 10000; i++) {
 		++standardHistogram[std::round(RandomBetween(first, last))];
 	}
 
@@ -60,37 +67,140 @@ int main()
 	return 0;
 }
 
-int RandomBetweenU(int first, int last, std::mt19937& r1) 
-{
+int RandomBetweenU(int first, int last, std::mt19937& e1) {
 
 	std::uniform_int_distribution<int> uniform_dist(first, last);
-	int ran = uniform_dist(r1);
+	int val = uniform_dist(e1);
 
-	return ran;
+	return val;
+
 }
 
-int RandomBetweenN(int first, int last, std::mt19937& r1) 
-{
-	std::normal_distribution<> normal_dist(2.5, 2);
-	int ran = normal_dist(r1);
+int RandomBetweenN(int first, int last, std::mt19937& e1) {
 
-	return ran;
+	std::normal_distribution<> normal_dist(4, 1), min(first), max(last);
+	int val = normal_dist(e1);
+
+	return val;
+
 }
 
-int RandomBetween(int first, int last) 
-{
-	int ran = (std::round(std::rand()));
-	ran = ran % 6;
-	return ran;
+int RandomBetween(int first, int last) {
+	int val = (std::round(std::rand()));
+	val = val % 6;
+	val++;
+	return val;
+
 }
 
-void PrintDistribution(const std::map<int, int>& numbers) 
-{
-	for (auto p : numbers)
-	{
-		std::cout << std::fixed << std::setprecision(1) 
-			<< std::setw(2) << p.first << ' ' << std::string(p.second / 20, '*') 
-			<< std::endl;
+void PrintDistribution(const std::map<int, int>& numbers) {
+
+	for (auto p : numbers) {
+		if (p.first > 1 || p.first < 6) {
+			cout << std::fixed << std::setprecision(1) << std::setw(2) << p.first << ' ' << std::string(p.second / 20, '*') << endl;
+		}
+		else {
+			cout << std::fixed << std::setprecision(1) << std::setw(2) << p.first << ' ' << "# of instances: " << p.second << endl;
+		}
 	}
-	std::cout << std::endl;
+
+	cout << endl;
 }
+
+
+//#include <iostream>
+//#include <iomanip>
+//#include <string>
+//#include <map>
+//#include <random>
+//#include <cmath>
+//#include <stdlib.h>
+//
+//int RandomBetweenU(int first, int last, std::mt19937& r1);
+//int RandomBetweenN(int first, int last, std::mt19937& r1);
+//int RandomBetween(int first, int last);
+//void PrintDistribution(const std::map<int, int>& numbers);
+//
+//int main()
+//{
+//	// Declare endpoints of the range for the distributions
+//
+//	int first = 1;
+//	int last = 6;
+//
+//	// Declare pseudo-random device for creating seeds
+//	std::random_device r;
+//
+//	// Create a seed sequence to feed to the generator
+//	std::seed_seq seedObj{ r(), r(), r(), r(), r(), r(), r(), r() };
+//
+//	// Declare random-number generator and provide with seedObj sequence
+//	std::mt19937 r1(seedObj);
+//
+//	// Create uniform historgram
+//	std::map<int, int> uniformHistogram;
+//	for (int i = 0; i < 10000; i++) 
+//	{
+//		++uniformHistogram[std::round(RandomBetweenU(first, last, r1))];
+//	}
+//
+//	// Print the uniform histogram
+//	PrintDistribution(uniformHistogram);
+//
+//	// Create normal histogram
+//	std::map<int, int> normalHistogram;
+//	for (int i = 0; i < 10000; i++) 
+//	{
+//		++normalHistogram[std::round(RandomBetweenN(first, last, r1))];
+//	}
+//
+//	// Print the normal histogram
+//	PrintDistribution(normalHistogram);
+//
+//	// Create standard rand() histogram
+//	std::map<int, int> standardHistogram;
+//	for (int i = 0; i < 10000; i++) 
+//	{
+//		++standardHistogram[std::round(RandomBetween(first, last))];
+//	}
+//
+//	// Print the normal histogram
+//	PrintDistribution(standardHistogram);
+//
+//	return 0;
+//}
+//
+//int RandomBetweenU(int first, int last, std::mt19937& r1) 
+//{
+//
+//	std::uniform_int_distribution<int> uniform_dist(first, last);
+//	int ran = uniform_dist(r1);
+//
+//	return ran;
+//}
+//
+//int RandomBetweenN(int first, int last, std::mt19937& r1) 
+//{
+//	std::normal_distribution<> normal_dist(2.5, 2);
+//	int ran = normal_dist(r1);
+//
+//	return ran;
+//}
+//
+//int RandomBetween(int first, int last) 
+//{
+//	int ran = (std::round(std::rand()));
+//	ran = ran % 6;
+//	return ran;
+//}
+//
+//void PrintDistribution(const std::map<int, int>& numbers) 
+//{
+//	for (auto p : numbers)
+//	{
+//		std::cout << std::fixed << std::setprecision(1) 
+//			<< std::setw(2) << p.first << ' ' << std::string(p.second / 20, '*') 
+//			<< std::endl;
+//	}
+//	std::cout << std::endl;
+//}
