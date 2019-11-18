2d2f61b2dc0d749068c7576c9de6d8a9ae338a63
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-17 18:54:55 -0900

    Added comments

--- hw6/hangman/hangman.cpp
+++ hw6/hangman/hangman.cpp
@@ -7,26 +7,33 @@
 
 std::string guess; //Stores user input.
 std::map<int, std::string> guesses; //Store users guesses and lives remaining. 
-std::vector<std::string> guesses_vec{"_", "_", "_", "_", "_", "_", "_", "_"}; //Stores the blank word.
+//std::vector<std::string> blank{"_", "_", "_", "_", "_", "_", "_", "_"}; //Stores the blank word.
 std::vector<std::string> words{ "d", "i", "n", "o", "s", "a", "u", "r" }; //Stores the actual word.
 int life = 0; //Stores number of chances left.
+std::string correctWord = "";
 
 //Function definitions.
 void printGuess(std::vector<std::string> words);
 bool findLetter(std::string guess);
-void printEmpty(std::vector<std::string> guesses_vec);
+//void printEmpty(std::vector<std::string> blank);
+//void replace(std::string guess);
 
 //Main.
 int main(int argc, char** argv)
 {
-	while (life <= 10) //Asks for user input.
+	while (correctWord != "dinosaur" && life <= 10) //Asks for user input.
 	{
 		std::cout << "Guess a letter: " << "\n";
 		std::getline(std::cin, guess);
 		life++;
 		guesses.insert(std::pair<int, std::string>(life, guess));
-		//guesses_vec.push_back(guess);
-		printEmpty(guesses_vec);
+		if (std::find(words.begin(), words.end(), guess) != words.end())
+		{
+			correctWord.append(guess);
+			std::cout << "Word so far: " << correctWord << "\n";
+		}
+		//blank.push_back(guess);
+		//printEmpty(blank);
 
 		//Outputs letters guessed so far and lives used.
 		std::cout << "Guesses so far: \n";
@@ -34,7 +41,8 @@ int main(int argc, char** argv)
 		for (it = guesses.begin(); it != guesses.end(); ++it)
 			std::cout << "Lives used: " << it->first << " - " << "Letters guessed: " << it->second << '\n';
 
-		if (life == 10)
+		//If word not correct and lives finished then user fails.
+		if (life == 10 && correctWord != "dinosaur")
 		{
 			std::cout << "\n";
 			std::cout << "You died!" << std::endl;
@@ -42,16 +50,24 @@ int main(int argc, char** argv)
 			printGuess(words);
 			break;
 		}
-		else
+		else //If lives are not finished then run function.
 		{
 			findLetter(guess);
+			//replace(guess);
+		}
+
+		//If user guesses word correctly then they win.
+		if (correctWord == "dinosaur")
+		{
+			std::cout << "You win!" << std::endl;
 		}
 	}
 	return 0;
 }
 
 
-void printGuess(std::vector<std::string> words)
+//Prints the word if the user fails. 
+void printGuess(std::vector<std::string> words) 
 {
 	// Lambda expression to print vector containing the word.
 	for_each (words.begin(), words.end(), [](std::string i)
@@ -61,37 +77,42 @@ void printGuess(std::vector<std::string> words)
 	std::cout << std::endl;
 }
 
-//Functions.
-void printEmpty(std::vector<std::string> guesses_vec)
-{
-	// Lambda expression to print vector containing the word.
-	for_each(guesses_vec.begin(), guesses_vec.end(), [](std::string i)
-		{
-			std::cout << i << " ";
-		});
-	std::cout << std::endl;
-}
 
+//Checks the letter entered by user, if it's there it tells the user it is. If not, 
+//it tells the user the letter is not in the word. 
 bool findLetter(std::string guess)
 {
 	auto result1 = std::find(std::begin(words), std::end(words), guess);
 
-	if (result1 != std::end(words)) 
+	if (result1 != std::end(words))
 	{
 		std::cout << "Word contains: " << guess << '\n';
 		return true;
 	}
-	else 
+	else
 	{
 		std::cout << "Word does not contain: " << guess << '\n';
 		return false;
 	}
 }
 
-//void replace()
+
+//Functions.
+//void printEmpty(std::vector<std::string> blank)
 //{
-//	if ()
+//	// Lambda expression to print vector containing the word.
+//	for_each(blank.begin(), blank.end(), [](std::string i)
+//		{
+//			std::cout << i << " ";
+//		});
+//	std::cout << std::endl;
+//}
+
+
+//void replace(std::string guess)
+//{
+//	if (findLetter)
 //	{
-//
+//		blank.push_back(guess);
 //	}
 //}
