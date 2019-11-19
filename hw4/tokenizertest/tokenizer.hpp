c1cf18da257a7a9c3ce768d8c32fc0b5a16b4416
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-18 17:06:02 -0900

    Cleaned up some code to make it look better/more readable

--- hw4/tokenizertest/tokenizer.hpp
+++ hw4/tokenizertest/tokenizer.hpp
@@ -11,13 +11,19 @@ Description: Identifying tokens for CS-201
 #include<sstream>
 
 bool ReadLine(std::string& str);
-unsigned StringToTokensWS(const std::string& input, std::vector<std::string>& tokens);
-void AnalyzeTokens(const std::vector<std::string>& tokens);
-void Print(std::string& token, const size_t& longest_length, std::string& potType);
+unsigned StringToTokensWS(const std::string& input, 
+	std::vector<std::string>& tokens);
+void AnalyzeTokens(const std::vector<std::string>& 
+	tokens);
+void Print(std::string& token, const size_t& 
+	longest_length, std::string& potType);
 void Escape(std::string& token, size_t& tokenLength);
-void String(std::string& token, std::string& potType, bool& res, const size_t& longest_length);
-void Whitespace(std::string& token, std::string& potType, bool& res, const size_t& longest_length);
-int Type(std::string& token, std::string& potType, bool& res, const size_t& longest_length, std::vector<char>& v);
+void String(std::string& token, std::string& potType, 
+	bool& res, const size_t& longest_length);
+void Whitespace(std::string& token, std::string& potType, 
+	bool& res, const size_t& longest_length);
+int Type(std::string& token, std::string& potType, bool& res, 
+	const size_t& longest_length, std::vector<char>& v);
 
 
 //unsigned StringToTokenWS(std::vector<std::string>& tokens);
