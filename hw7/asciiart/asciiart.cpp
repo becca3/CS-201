05f804a58515cf8084d21b7b9df1b1433475b9d3
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-21 10:55:11 -0900

    Added some comments

--- hw7/asciiart/asciiart.cpp
+++ hw7/asciiart/asciiart.cpp
@@ -1,7 +1,12 @@
 #include <iostream> 
-#include "asciiart.h"
+
+//#include "asciiart.h"
+#include "Image3.hpp"
+#include "Color3.hpp"
 
 int main(int argc, char** argv)
 {
+	//Color3 x; //Declare an instance of class Color3.
+
 	return 0;
 }
