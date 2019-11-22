766c439225d130579351f2cb68e054d80ce494c5
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-21 17:19:57 -0900

    Added for loops to print the code according to rules

--- hw7/rule30/Project1/rule30.cpp
+++ hw7/rule30/Project1/rule30.cpp
@@ -5,11 +5,11 @@
 
 int main()
 {
-	int x;
-	int y;
-	int z;
+	int x; //i
+	int y; //j 
+	int z; //n
 	int step_num;
-	char* a;
+	char* a; //x
 	char* a_old;
 
 	std::cout << "\n";
@@ -32,6 +32,28 @@ int main()
 		std::cout << a[x];
 	}
 	std::cout << "\n";
+
+	for (y = 1; y <= step_num; y++)
+	{
+		for (x = 0; x < z + 2; x++)
+		{
+			a_old[x] = a[x];
+		}
+		for (x = 1; x <= z; x++)
+		{
+			if ((a_old[x - 1] == ' ' && a_old[x] == ' ' && a_old[x + 1] == '*') ||
+				(a_old[x - 1] == ' ' && a_old[x] == '*' && a_old[x + 1] == ' ') ||
+				(a_old[x - 1] == ' ' && a_old[x] == '*' && a_old[x + 1] == '*') ||
+				(a_old[x - 1] == '*' && a_old[x] == ' ' && a_old[x + 1] == ' '))
+			{
+				a[x] = '*';
+			}
+			else
+			{
+				a[x] = ' ';
+			}
+		}
+	}
 
 	return 0;
 }
