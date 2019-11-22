c6e3120c33006f5c0efcc3205d784daaeccab013
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-22 12:46:55 -0900

    Lab 34

---
+++ Labs/034/034/printtime.cpp
@@ -0,0 +1,65 @@
+// printtime.cpp  INCOMPLETE
+// Glenn G. Chappell
+// 20 Apr 2018
+//
+// For CS 201 Spring 2018
+// Time of Day Class & Program
+//
+// Modified 11/28/18 for CS 201 Fall 2018
+// Chris Hartman
+
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+// Class Time
+// Holds a time of day: hours, minutes, seconds. Can print its stored
+// time in a nice format.
+class Time {
+
+public:
+// ***** Time: constructors *****
+
+    // TODO: Put something here!
+	Time() : _hr(12), _min(34), _sec(20) {}
+		
+// ***** Time: general public member functions *****
+
+    void print() const
+    {
+		cout << std::setfill(':') << std::setw(2);
+		cout << _hr << ":" << _min << ":" << _sec;
+    }
+
+// ***** Time: data members *****
+private:
+
+    int _hr;   // Hours past midnight
+    int _min;  // Minutes past the hour
+    int _sec;  // Seconds past the minute
+
+};  // End class Time
+
+
+int main()
+{
+    // Print header
+    cout << "Here are some times:" << endl;
+    cout << endl;
+
+
+    // Make a Time object; print it
+    Time t1;
+    cout << "Time #1: [";
+    t1.print();
+    cout << "]" << endl;
+    cout << endl;
+
+    // Wait for user
+    cout << "PRESS ENTER to quit ";
+    while (cin.get() != '\n') ;
+}
+
