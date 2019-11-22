8fcacfbb40a0d47a917c4c6d5076df1de6bbc853
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-21 16:59:45 -0900

    Created some variables for rule30.cpp

---
+++ hw7/rule30/Project1/rule30.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+# include <ctime>
+# include <stdlib.h>
+
+
+int main()
+{
+	int i;
+	int j;
+	int n;
+	int step_num;
+	char* x;
+	char* x_old;
+
+	std::cout << "\n";
+	std::cout << "CELLULAR AUTOMATON:\n";
+
+	return 0;
+}
