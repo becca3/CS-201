87cb965fdb911a310742ef98bbb9b5c64897e119
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-21 17:52:05 -0900

    Need to implement the vector bit then it's done

--- hw7/rule30/Project1/rule30.cpp
+++ hw7/rule30/Project1/rule30.cpp
@@ -1,18 +1,21 @@
 /*
-Program: rule30.cpp
-Author: Rebecca Morgan
-Date: 11/21/2019
-Description: Rule30 program for CS-201
+Program: rule30.cpp.
+Author: Rebecca Morgan.
+Date: 11/21/2019.
+Description: Rule30 program for CS-201.
 */
 
-# include <iostream>
-# include <ctime>
-# include <stdlib.h>
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <vector>
+#include <string>
 
+std::vector<std::string> txt;
 
-int main()
+int main(int argc, char** argv)
 {
-	//Variables
+	//Variables.
 	int a;
 	int b;
 	int c;
@@ -20,21 +23,25 @@ int main()
 	char* x;
 	char* x_old;
 
-	//Intro
+	//Intro.
 	std::cout << "\n";
 	std::cout << "CELLULAR AUTOMATON:\n";
 
-	c = 80;
-	step_num = 80;
+	//Initialisation of variables.
+	c = 40;
+	step_num = 20;
 
 	x = new char[c + 2];
 	x_old = new char[c + 2];
 
+
+	//For loop beginning the rules.
 	for (a = 0; a <= c + 1; a++)
 	{
 		x[a] = ' ';
 	}
-	x[40] = '*';
+
+	x[20] = '*';
 
 	for (a = 1; a <= c; a++)
 	{
@@ -70,14 +77,16 @@ int main()
 		for (a = 1; a <= c; a++)
 		{
 			std::cout << x[a];
+			txt.push_back(x);
 		}
+
+		
 		std::cout << "\n";
 	}
 
 	//Reset the pointers. 
 	delete[] x;
 	delete[] x_old;
-
 
 	std::cout << "\n";
 
