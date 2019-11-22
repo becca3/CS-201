e37fba550bb64c202dfad298fbb9602a0dbce36b
becca3 <32517979+becca3@users.noreply.github.com>
2019-11-21 17:02:15 -0900

    Added some calculaitons and more variables

--- hw7/rule30/Project1/rule30.cpp
+++ hw7/rule30/Project1/rule30.cpp
@@ -5,15 +5,21 @@
 
 int main()
 {
-	int i;
-	int j;
-	int n;
+	int x;
+	int y;
+	int z;
 	int step_num;
-	char* x;
-	char* x_old;
+	char* a;
+	char* a_old;
 
 	std::cout << "\n";
 	std::cout << "CELLULAR AUTOMATON:\n";
+
+	z = 80;
+	step_num = 80;
+
+	a = new char[z + 2];
+	a_old = new char[z + 2];
 
 	return 0;
 }
