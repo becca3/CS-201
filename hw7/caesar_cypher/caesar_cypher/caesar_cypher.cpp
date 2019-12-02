20410b865d4a91e5003f003c85cf2f32d8acd092
becca3 <32517979+becca3@users.noreply.github.com>
2019-12-02 14:50:32 -0900

    Added caesar cypher program to hw7

--- hw7/caesar_cypher/caesar_cypher/caesar_cypher.cpp
+++ hw7/caesar_cypher/caesar_cypher/caesar_cypher.cpp
@@ -1,15 +1,94 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 
-const char Alphabet[] = {
-	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
-	'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-	'W', 'X', 'Y', 'Z' };
-
-int main(int arcg, char** argv)
+int main()
 {
-	for (auto i : Alphabet)
+	//Variables.
+	int i;
+	int j;
+	int length;
+	int choice;
+	int key;
+	char msg[100];
+	std::string inp;
+
+	std::cout << "play or quit? \n";
+	std::cin >> inp;
+
+	if (inp == "quit")
 	{
-		std::cout << "Letter is: " << i << std::endl;
+		return 0;
+	}
+	else
+	{
+		//Ask for user input.
+		std::cout << "Enter the message: \n";
+		std::cin >> msg;
+		//std::cin.getline(msg, 100);
+		std::cout << "Enter key: ";
+		std::cin >> key;
+		length = strlen(msg);
+		std::cout << "Enter your choice \n1. Encryption \n2. Decryption \n";
+		std::cin >> choice;
+
+		if (choice == 1) //Encrypt. 
+		{
+			char character;
+			for (int i = 0; msg[i] != '\0'; ++i)
+			{
+				character = msg[i];
+				//Encrypt lowercase letters.
+				if (character >= 'a' && character <= 'z')
+				{
+					character = character + key;
+					if (character > 'z')
+					{
+						character = character - 'z' + 'a' - 1;
+					}
+					msg[i] = character;
+				}
+				//Encrypt uppercase letters.
+				else if (character >= 'A' && character <= 'Z')
+				{
+					character = character + key;
+					if (character > 'Z')
+					{
+						character = character - 'Z' + 'A' - 1;
+					}
+					msg[i] = character;
+				}
+			}
+			std::cout << "Encrypted message: " << msg << "\n";
+		}
+		else if (choice == 2) //Decrypt.
+		{
+			char character;
+			for (int i = 0; msg[i] != '\0'; ++i)
+			{
+				character = msg[i];
+				//Decrypt lowercase letters.
+				if (character >= 'a' && character <= 'z')
+				{
+					character = character - key;
+					if (character < 'a')
+					{
+						character = character + 'z' - 'a' + 1;
+					}
+					msg[i] = character;
+				}
+				//Decrypt uppercase letters.
+				else if (character >= 'A' && character <= 'Z')
+				{
+					character = character - key;
+					if (character < 'A')
+					{
+						character = character + 'Z' - 'A' + 1;
+					}
+					msg[i] = character;
+				}
+			}
+			std::cout << "Decrypted message: " << msg << "\n";
+		}
 	}
 	return 0;
 }
