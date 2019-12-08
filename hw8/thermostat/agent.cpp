7d8cbece57eb836a35862e0a5d9f793e225f7a53
becca3 <32517979+becca3@users.noreply.github.com>
2019-12-08 14:02:49 -0900

    Added functions for agent.cpp

--- hw8/thermostat/agent.cpp
+++ hw8/thermostat/agent.cpp
@@ -1,2 +1,31 @@
 #include "agent.h"
 
+Agent::Agent(int pInt)
+{
+	desired_temp = pInt;
+}
+
+int Agent::perceive(const Enviornment& pEnv)
+{
+	return pEnv.getTemp();
+}
+
+bool Agent::think(const Enviornment& pEnv)
+{
+	int current_temp = perceive(pEnv);
+
+	if (current_temp >= desired_temp) //Too hot.
+		return false; //Heater off
+
+	return true; //Too cold = heater on
+}
+
+void Agent::setDesiredTemp(int pInt)
+{
+	desired_temp = pInt;
+}
+
+void Agent::act(Enviornment& pEnv)
+{
+	pEnv.setHeaterState(think(pEnv));
+}
