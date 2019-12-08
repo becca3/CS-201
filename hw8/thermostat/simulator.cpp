131d78ffb70ebbf1b0199039a935e2a001784828
becca3 <32517979+becca3@users.noreply.github.com>
2019-12-08 14:26:13 -0900

    Added things for simulator.cpp

--- hw8/thermostat/simulator.cpp
+++ hw8/thermostat/simulator.cpp
@@ -1,3 +1,20 @@
 #include "simulator.h"
 
 #include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::getline;
+
+#include <string>
+using std::string;
+using std::stoi;
+
+#include <stdexcept>
+
+string Simulator::getInput()
+{
+	string input = "";
+	getline(cin, input);
+	return input;
+}
