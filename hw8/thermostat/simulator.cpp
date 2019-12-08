c4f01615104c8b72b0b32fd8a00695f81b5b3cfb
becca3 <32517979+becca3@users.noreply.github.com>
2019-12-08 14:40:40 -0900

    Added more functions to simulator.cpp

--- hw8/thermostat/simulator.cpp
+++ hw8/thermostat/simulator.cpp
@@ -18,3 +18,51 @@ string Simulator::getInput()
 	getline(cin, input);
 	return input;
 }
+
+void Simulator::askOwner(Agent& pAg, Enviornment& pEnv)
+{
+	bool loop2 = true;
+	string input = "";
+	int intInput = 0;
+
+	while (loop2)
+	{
+		cout << "Enter a desired temperature, or enter \"QUIT\" to quit." << endl;
+		input = getInput();
+
+		try
+		{
+			intInput = stoi(input);
+			cout << "Desired temperature: " << intInput << endl;
+			pAg.setDesiredTemp(intInput);
+			loop2 = false;
+		}
+		catch (const std::invalid_argument& ia)
+		{
+			if (input == "QUIT")
+			{
+				cout << "Quiting." << endl;
+				loop = false;
+				loop2 = false;
+			}
+			else
+			{
+				cout << "Invalid input. Try again." << endl;
+				loop2 = true;
+			}
+		}
+	}
+}
+
+void Simulator::run(Agent& pAg, Enviornment& pEnv)
+{
+	while (loop)
+	{
+		for (int i = 0; i < 10; ++i)
+		{
+			pEnv.iteration();
+			pAg.act(pEnv);
+		}
+		askOwner(pAg, pEnv);
+	}
+}
